677e62075becaaffd6c795fd2525faa56c314223
Travis Hotz <travishotz@gmail.com>
2012-03-30 16:50:12 -0700

    Created general flow, game scoring still not functional

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,12 @@ class GamesController < ApplicationController
   
   def create
     @game = Game.create(params[:game])
+    redirect_to new_game_path # this is a bit messy/nonfunctional
+  end
+  
+  def index
+    @users = User.all
+    @game = Game.new(:player_x_id => params["player_x_id"], :player_o_id => params["player_o_id"])
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
   
   def create
-    User.create(params[:user])
+    @user = User.create(params[:user])
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
   end
   
   def index
     @users = User.all.sort{|u1,u2| u1.wins.size <=> u2.wins.size}
   end
   
+  def new
+    @user = User.new(params[:user])
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 end
