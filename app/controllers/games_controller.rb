572118ad7854163fb7bb791d93cbed385c637e32
Travis Hotz <travishotz@gmail.com>
2012-03-30 17:00:18 -0700

    Add manual win loss fields on game/new but not working yet

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
   
   def new
-    @game = Game.new(:player_x_id => params["player_x_id"], :player_o_id => params["player_o_id"])
+    @game = Game.new(:player_x_id => params["player_x_id"], :player_o_id => params["player_o_id"], :score => 0)
+    @users = User.all #this should be just the two users passed from the new game view
+    # if @game.save
+    #  redirect_to users_path
+    # end
   end
   
   def create
